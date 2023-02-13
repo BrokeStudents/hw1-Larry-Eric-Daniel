6209f889201a570e10e3631ace192ee04dd28bb4
eric37456 <eric37456@gmail.com>
2023-02-13 07:08:59 -0800

    Resetting mistake

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Apportionment
+{
+public:
+    Apportionment()
+    {
+        max_rep = 435;
+    };
+    ~Apportionment()
+    {
+        input_file.close();
+        output_file.close();
+    };
+    void input() {
+
+        cout << "Enter the csv file name with .csv extension. " << endl;
+        cin >> fileName;
+        input_file.open(fileName);
+        if (!input_file.is_open())
+            cout << "Error: File Open" << endl;
+
+    };
+    void output() {
+        while (input_file.good()) {
+
+            string line1;
+            string line2;
+            getline(input_file, line1, ',');
+            getline(input_file, line2, '\n');
+
+            cout << line1 << " " << line2 << endl;
+        }
+
+    };
+    int totalPopulation() const {
+        return 0;
+    };
+<<<<<<< HEAD
+    int avgRepPerPop() const {
+        return 0;
+=======
+
+    int avgRepPerPop() const  {
+        int population = totalPopulation();
+
+        return population / max_rep;
+
+
+>>>>>>> parent of 8da32bf (Made changes)
+    };
+    float representatives() {
+        return 0;
+    };
+    void distribute() {
+
+    };
+
+private:
+    int max_rep;
+    string fileName;
+    ifstream input_file;
+    ofstream output_file;
+};
+
+int main()
+{
+    Apportionment test1;
+    test1.input();
+    test1.output();
+    return 0;
+}
