2840d7360252d2fed62b45d72e37bdd0d3b54432
eric37456 <eric37456@gmail.com>
2023-02-12 20:30:42 -0800

    Wrote function for calcualting representatives, floor value, remainder. Changed to void function for convenience.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 class Apportionment
@@ -22,38 +26,75 @@ public:
         input_file.open(fileName);
         if (!input_file.is_open())
             cout << "Error: File Opening failed" << endl;
-
-    };
+    }
     void output() {
         while (input_file.good()) {
-
             string line1;
             string line2;
             getline(input_file, line1, ',');
             getline(input_file, line2, '\n');
-
+          
             cout << line1 << " " << line2 << endl;
+            
+            numbers.push_back(atoi(line2.c_str())); //Convert line2 to an integer and store them in a vector
+         
+        }
+    }
+    int totalPopulation() const {
+
+        int population = 0;
+
+        for (auto it = numbers.begin(); it != numbers.end(); ++it) {
+            population += *it; 
+        }
+
+            
+
+        return population;
+    };
+
+    int avgRepPerPop() const {
+        int population = totalPopulation();
+
+        return population / max_rep;
+
+    
+    };
+
+    void representatives() {
+
+        int average = avgRepPerPop();
+
+        for (int i = 0; i < 50; i ++) {
+
+            number_of_Representatives[i] =  numbers[i] / static_cast<float>(average);
+
+            floorValue[i] = floor(number_of_Representatives[i]);
+
+            remainder[i] = number_of_Representatives[i] - floorValue[i];
+
+            cout << '\n' <<" Representatives: " << number_of_Representatives[i] <<" floorValue : " << floorValue[i]  <<" remainder : " << remainder[i] << '\n';
+
         }
 
     };
-    int totalPopulation() const {
-        return 0;
-    };
-    int avgRepPerPop() const {
-        return 0;
-    };
-    float representatives() {
-        return 0;
-    };
+
+    
     void distribute() {
 
+        return 0;
     };
-
+    
 private:
     int max_rep;
     string fileName;
     ifstream input_file;
     ofstream output_file;
+    vector <int> numbers;
+    float number_of_Representatives[50];
+    float remainder[50];
+    int floorValue[50];
+
 };
 
 int main()
@@ -61,5 +102,11 @@ int main()
     Apportionment test1;
     test1.input();
     test1.output();
+
+    cout << '\n' << "Total Population: " << test1.totalPopulation();
+    cout << '\n' << "Population average: " << test1.avgRepPerPop();
+
+    test1.representatives();
+    
     return 0;
 }
