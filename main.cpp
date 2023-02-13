0f940a5f5d6e598caa6ac1864931ce46a1806692
eric37456 <eric37456@gmail.com>
2023-02-12 20:29:06 -0800

    Implement method for calulating representatives, floor value, remainder. Changed to void function for convenience.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <string>
-#include <vector>
-#include <algorithm>
-
 using namespace std;
 
 class Apportionment
@@ -26,52 +22,29 @@ public:
         input_file.open(fileName);
         if (!input_file.is_open())
             cout << "Error: File Opening failed" << endl;
-    void input() {
-
-        cout << "Enter the csv file name with .csv extension. " << endl;
-        cin >> fileName;
-        input_file.open(fileName);
-        if (!input_file.is_open())
-            cout << "Error: File Open" << endl;
 
     };
     void output() {
         while (input_file.good()) {
+
             string line1;
             string line2;
             getline(input_file, line1, ',');
             getline(input_file, line2, '\n');
-          
+
             cout << line1 << " " << line2 << endl;
-            
-            numbers.push_back(atoi(line2.c_str())); //Convert line2 to an integer and store them in a vector
-         
         }
 
-    int totalPopulation() const {
-
-        int population = 0;
-
-        for (auto it = numbers.begin(); it != numbers.end(); ++it) {
-            population += *it; 
-        }
-
-            
-
-        return population;
     };
-
+    int totalPopulation() const {
+        return 0;
+    };
     int avgRepPerPop() const {
         return 0;
-    
     };
-
-    float representatives() {
-        return 0;
     float representatives() {
         return 0;
     };
-    void distribute() {
     void distribute() {
 
     };
@@ -81,11 +54,6 @@ private:
     string fileName;
     ifstream input_file;
     ofstream output_file;
-    vector <int> numbers;
-    float number_of_Representatives[50];
-    float remainder[50];
-    int floorValue[50];
-
 };
 
 int main()
@@ -93,8 +61,5 @@ int main()
     Apportionment test1;
     test1.input();
     test1.output();
-
-    cout << '\n' << "Total Population: " << test1.totalPopulation();
-
     return 0;
 }
