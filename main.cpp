56f23f5dd8f7f900b6196cd5618367eba9db8d39
eric37456 <eric37456@gmail.com>
2023-02-12 16:36:29 -0800

    Method for calculating the total population also added a few headers and variables

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 class Apportionment
@@ -14,32 +18,72 @@ public:
         input_file.close();
         output_file.close();
     };
-    void input(){
+    void input() {
+
+        cout << "Enter the csv file name with .csv extension. " << endl;
+        cin >> fileName;
+        input_file.open(fileName);
+        if (!input_file.is_open())
+            cout << "Error: File Open" << endl;
 
     };
-    void output(){
+    void output() {
+        while (input_file.good()) {
+            string line1;
+            string line2;
+            getline(input_file, line1, ',');
+            getline(input_file, line2, '\n');
+          
+            cout << line1 << " " << line2 << endl;
+            
+            numbers.push_back(atoi(line2.c_str())); //Convert line2 to an integer and store them in a vector
+         
+        }
 
     };
     int totalPopulation() const {
 
+        int population = 0;
+
+        for (auto it = numbers.begin(); it != numbers.end(); ++it) {
+            population += *it; 
+        }
+
+            
+
+        return population;
     };
+
     int avgRepPerPop() const {
-
+        return 0;
     };
-    float representatives(){
 
+    float representatives() {
+        return 0;
     };
-    void distribute(){
+    void distribute() {
 
     };
 
 private:
     int max_rep;
+    string fileName;
     ifstream input_file;
     ofstream output_file;
+    vector <int> numbers;
+    float number_of_Representatives[50];
+    float remainder[50];
+    int floorValue[50];
+
 };
 
 int main()
 {
+    Apportionment test1;
+    test1.input();
+    test1.output();
+
+    cout << '\n' << "Total Population: " << test1.totalPopulation();
+
     return 0;
 }
